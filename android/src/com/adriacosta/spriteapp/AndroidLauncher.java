1994d86acf38cd1eef3e1f4f3d9543b1e47dd915
A80167811050629\alum-01 <adriacostasalse@gmail.com>
2022-02-02 19:58:47 +0100

    App final

---
+++ android/src/com/adriacosta/spriteapp/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.adriacosta.spriteapp;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.adriacosta.spriteapp.SpriteClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpriteClass(), config);
+	}
+}
